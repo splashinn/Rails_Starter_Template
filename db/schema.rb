c666b9446db2bed2d59dd5ce5f461aa8d13ca3b0
Kyle Maune <kmaune71@gmail.com>
2014-10-09 20:44:21 -0700

    started the code for tagging

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141010025154) do
+ActiveRecord::Schema.define(version: 20141010034101) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -29,5 +29,21 @@ ActiveRecord::Schema.define(version: 20141010025154) do
   end
 
   add_index "comments", ["article_id"], name: "index_comments_on_article_id"
+
+  create_table "taggings", force: true do |t|
+    t.integer  "tag_id"
+    t.integer  "articles_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "taggings", ["articles_id"], name: "index_taggings_on_articles_id"
+  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id"
+
+  create_table "tags", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
